973ff921070efd4116b8db0e261fa8cb3ba3a5f5
bartoszpogoda <bartek.pogoda1@gmail.com>
2018-05-27 21:15:09 +0200

    updated test

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,13 @@
 from painterAssistant import paintCanCalculator
-
+from painterAssistant import paintCanInfo
 
 def test_number_of_paint_cans_calculated_correctly_1():
     """
     make sure number of paint cans needed is calculated corectly
     """
 
-    assert paintCanCalculator.how_many_needed(5, 5, 5) == 5, \
+    info = paintCanInfo.PaintCanInfo(0)
+    info.efficiency = 5
+
+    assert paintCanCalculator.how_many_needed(5, 5, info) == 5, \
         'Result for 5x5 wall and 5m^2 paint can efficiency should should be 5!'
